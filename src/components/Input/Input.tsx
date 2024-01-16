9093ff3665feb116bcd949874ce790c3abf26b86
Roberto Oliveira <roberto.oliveira@toughtworks.com>
2024-01-16 09:43:10 -0300

    feat/components: update input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View} from 'react-native';
+import {TouchableOpacity, View} from 'react-native';
 import styled from 'styled-components/native';
 import Icon from '../Icon/Icon';
 
@@ -8,6 +8,7 @@ interface CustomInputProps {
   label: string;
   onChangeText: (text: string) => void;
   type?: 'text' | 'password';
+  capitonPassword?: string;
 }
 
 const Container = styled.View`
@@ -47,11 +48,20 @@ const Label = styled.Text`
   margin-bottom: 8px;
 `;
 
+const CaptionPassword = styled(Label)`
+  color: #cacaca;
+  text-align: right;
+  margin-top: 8px;
+  font-weight: 500;
+  line-height: 16px;
+`;
+
 const Input: React.FC<CustomInputProps> = ({
   placeholder,
   label,
   onChangeText,
   type = 'text',
+  capitonPassword,
 }) => {
   const [inputValue, setInputValue] = useState<string>('');
 
@@ -73,6 +83,9 @@ const Input: React.FC<CustomInputProps> = ({
           <Icon name="Eyes" width={20} height={20} />
         </IconStyled>
       )}
+      <TouchableOpacity>
+        <CaptionPassword>{capitonPassword}</CaptionPassword>
+      </TouchableOpacity>
     </Container>
   );
 };
