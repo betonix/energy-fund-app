8cd93aa5f245dbffda958f5d37a8fffe324c1274
Roberto Oliveira <roberto.oliveira@toughtworks.com>
2024-01-16 21:12:59 -0300

    feat/home-page: create home page with chart

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -3,6 +3,9 @@ import Illustration from '../../assets/icons/Illustration.svg';
 import Eyes from '../../assets/icons/eyes.svg';
 import Visa from '../../assets/icons/visa.svg';
 import Send from '../../assets/icons/Send.svg';
+import Currency from '../../assets/icons/currency.svg';
+import Water from '../../assets/icons/water.svg';
+import Banana from '../../assets/icons/Banana.svg';
 
 type IconProps = {
   name: string;
@@ -16,6 +19,9 @@ const Icon = ({name, height, width}: IconProps) => {
     Eyes: String(Eyes),
     Visa: String(Visa),
     Send: String(Send),
+    Currency: String(Currency),
+    Water: String(Water),
+    Banana: String(Banana),
   };
 
   type IconOption = keyof typeof icons;
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {ScrollView, Text} from 'react-native';
 import {
   BalanceText,
   ButtonSendMoney,
@@ -17,6 +17,7 @@ import {
 } from './home.styles';
 import Icon from '../../components/Icon/Icon';
 import styled from 'styled-components/native';
+import LineChartGraph from '../../components/line-chart/line-chart';
 
 const BadgeOneCard = styled.View`
   border-radius: 10px;
@@ -50,34 +51,216 @@ const WrapperCard = styled.View`
   margin-bottom: 150px;
 `;
 
-const Home: React.FC = () => {
+const CaptionMoney = styled.Text`
+  color: #343434;
+  font-family: poppins;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 16px;
+`;
+
+const CaptionDate = styled.Text`
+  color: #979797;
+  font-family: poppins;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 16px;
+`;
+
+const ContainerChart = styled.View`
+  margin-top: 46px;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 8px;
+`;
+
+const BalanceChart = styled.Text`
+  color: #3629b7;
+  font-family: poppins;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 28px;
+  margin-bottom: 8px;
+`;
+
+const Currency = styled.Text`
+  color: #979797;
+  font-family: poppins;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 16px;
+`;
+
+const ContainerExtract = styled.View`
+  margin-top: 50px;
+`;
+const LabelExtract = styled.Text`
+  color: #979797;
+  font-family: poppins;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 16px;
+`;
+
+const ContainerItemExtract = styled.View`
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  margin-top: 12px;
+  margin-bottom: 12px;
+`;
+
+const TextExtract = styled.Text`
+  color: #343434;
+  font-family: poppins;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+  margin-right: 'auto';
+`;
+
+const ValuExtract = styled.Text`
+  color: ${({isPositive}) => (isPositive ? '#1CB999' : '#343434')};
+
+  text-align: right;
+  font-family: poppins;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px;
+  flex: 1;
+`;
+
+const ContainerIcon = styled.View`
+  background-color: ${({color}) => color};
+  width: 40px;
+  height: 40px;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+  margin-right: 12px;
+`;
+
+const Wrapper = styled.View`
+  border-bottom-width: 1px;
+  border-bottom-color: #ececec;
+  margin-top: 23px;
+`;
+
+type ItemExtractType = {
+  title: string;
+  data: Array<ItemExtractComponent>;
+};
+
+type ItemExtractComponent = {
+  icon: string;
+  value: string;
+  color: string;
+  text: string;
+  iscomming: boolean;
+};
+
+const ItemExtract: React.FC<ItemExtractType> = ({title, data}) => {
   return (
-    <Container>
-      <TitlePage>Current account</TitlePage>
-      <BalanceText>$3469.52</BalanceText>
-      <ContentPage>
-        <WrapperCard>
-          <ContainerCard>
-            <BadgeOneCard />
-            <BadgeTwoCard />
-            <CreditCard>
-              <UserName>John Smith</UserName>
-              <NameCard>Crowdform Platinium</NameCard>
-              <NumberCard>4756 **** **** 9018</NumberCard>
-              <ContainerBrand>
-                <Icon name="Visa" width={46} height={15} />
-              </ContainerBrand>
-              <CircleLeftCreditCard />
-              <CircleRightCreditCard />
-            </CreditCard>
-          </ContainerCard>
-        </WrapperCard>
-        <ButtonSendMoney>
-          <Icon name="Send" width={20} height={20}></Icon>
-          <TextSendMoney>Send money</TextSendMoney>
-        </ButtonSendMoney>
-      </ContentPage>
-    </Container>
+    <Wrapper>
+      <LabelExtract>{title}</LabelExtract>
+      {data.map(item => (
+        <ContainerItemExtract>
+          <ContainerIcon color={item.color}>
+            <Icon name={item.icon} width={15} height={15} />
+          </ContainerIcon>
+          <TextExtract>{item.text}</TextExtract>
+          <ValuExtract isPositive={item.iscomming}>{item.value}</ValuExtract>
+        </ContainerItemExtract>
+      ))}
+    </Wrapper>
+  );
+};
+
+const Home: React.FC = () => {
+  const [balance, setBalance] = useState();
+
+  const dataToday = [
+    {
+      icon: 'Currency',
+      text: 'Income: Salary Oct',
+      value: '+$1200',
+      color: '#FF4267',
+      iscomming: true,
+    },
+  ];
+
+  const dataPass = [
+    {
+      icon: 'Water',
+      text: 'Water Bill',
+      value: '$280',
+      color: '#3629B7',
+      iscomming: false,
+    },
+    {
+      icon: 'Banana',
+      text: 'Groceries',
+      value: '$32',
+      color: '#29B784',
+      iscomming: false,
+    },
+  ];
+
+  return (
+    <ScrollView>
+      <Container>
+        <TitlePage>Current account</TitlePage>
+        <BalanceText>$3469.52</BalanceText>
+        <ContentPage>
+          <WrapperCard>
+            <ContainerCard>
+              <BadgeOneCard />
+              <BadgeTwoCard />
+              <CreditCard>
+                <UserName>John Smith</UserName>
+                <NameCard>Crowdform Platinium</NameCard>
+                <NumberCard>4756 **** **** 9018</NumberCard>
+                <ContainerBrand>
+                  <Icon name="Visa" width={46} height={15} />
+                </ContainerBrand>
+                <CircleLeftCreditCard />
+                <CircleRightCreditCard />
+              </CreditCard>
+            </ContainerCard>
+          </WrapperCard>
+          <ButtonSendMoney>
+            <Icon name="Send" width={20} height={20}></Icon>
+            <TextSendMoney>Send money</TextSendMoney>
+          </ButtonSendMoney>
+          <ContainerChart>
+            <CaptionMoney>Insights</CaptionMoney>
+            <CaptionDate>20 Jan 24</CaptionDate>
+          </ContainerChart>
+          <BalanceChart>
+            {balance} <Currency>USD</Currency>
+          </BalanceChart>
+          <LineChartGraph onChange={value => setBalance(value)} />
+          <ContainerExtract>
+            {ItemExtract({
+              title: 'Today',
+              data: dataToday,
+            })}
+
+            {ItemExtract({
+              title: 'Yesterday',
+              data: dataPass,
+            })}
+          </ContainerExtract>
+        </ContentPage>
+      </Container>
+    </ScrollView>
   );
 };
 
